426b00c380f021af4a4b3bbdaa7f35d7a6f01035
Matthew Freeman <matthew.freeman821@gmail.com>
2021-02-22 16:24:40 -0500

    Updates value of guess

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
