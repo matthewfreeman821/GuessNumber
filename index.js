cd2d1459d461c6dfaf6cd6648ae7c3a235ae702a
Matthew Freeman <matthew.freeman821@gmail.com>
2021-02-22 16:22:23 -0500

    Selects the secret number and the score and changes to hardcoded values

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 'use strict';
 
 document.querySelector('.message').textContent = '🎉 Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
