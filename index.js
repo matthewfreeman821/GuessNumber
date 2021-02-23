c71e7247c2a3662bdc0f8b684a7c0ddeda425952
Matthew Freeman <matthew.freeman821@gmail.com>
2021-02-23 16:20:58 -0500

    Commented all previous js code. To be deleted later

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 23;
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+
