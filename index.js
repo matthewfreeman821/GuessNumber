99b7707ff3adc90bbc98806d1575dcccc53bdaae
Matthew Freeman <matthew.freeman821@gmail.com>
2021-02-24 16:06:49 -0500

    Adds selector to message and changes text content

--- index.js
+++ index.js
@@ -7,4 +7,6 @@
 
 document.querySelector('.check').addEventListener('click', function () {
   console.log(document.querySelector('.guess').value);
+
+  document.querySelector('.message').textContent = '🎉 Correct Number!';
 });
