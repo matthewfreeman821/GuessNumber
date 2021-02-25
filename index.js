9045b496ae439758354123edbb23feeece3e0bca
Matthew Freeman <matthew.freeman821@gmail.com>
2021-02-25 16:01:08 -0500

    Displays the secret number on the page for development purposes. Will hide again later

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // document.querySelector('.guess').value = 23;
 
 const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
