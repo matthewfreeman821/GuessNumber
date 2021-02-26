a08f47845d0a39f758142656ffc5f93c16a0685b
Matthew Freeman <matthew.freeman821@gmail.com>
2021-02-26 16:54:45 -0500

    Adds logic to handle if guess is too low

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = '📈 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
   }
 
 });
