244c8bb4527c79bc28b9ff7ea7381af97ecc98ab
Matthew Freeman <matthew.freeman821@gmail.com>
2021-02-27 13:44:14 -0500

    Adds logic if the score goes to 0 to display a message of losing the game

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!'
+    }
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = '📉 Too low!';
     score--;
