36a7a294e2c03d378373e6b4b5a062b573cf30dc
Matthew Freeman <matthew.freeman821@gmail.com>
2021-02-28 13:22:25 -0500

    Adds logic for when guess is too low to mimic when guess is too high

--- index.js
+++ index.js
@@ -26,9 +26,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!'
+      document.querySelector('.score').textContent = 0;
+    }
   }
 
 });
