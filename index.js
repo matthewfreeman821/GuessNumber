5603432de0f911f7749be38acc7ea55503da8d42
Matthew Freeman <matthew.freeman821@gmail.com>
2021-03-01 16:06:19 -0500

    Changes size of number display box on correct guess

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ document.querySelector('.check').addEventListener('click', function () {
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.message').style.width = '30rem';
   }
   // Guess is higher than number
   else if (guess > secretNumber) {
