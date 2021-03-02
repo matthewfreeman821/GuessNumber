d28adf15d437dddbf1a6eb80d5ecf77e92541b54
Matthew Freeman <matthew.freeman821@gmail.com>
2021-03-02 15:53:28 -0500

    Makes the secret number only visible upon winning the game

--- index.js
+++ index.js
@@ -7,7 +7,6 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -20,6 +19,7 @@ document.querySelector('.check').addEventListener('click', function () {
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
   }
   // Guess is higher than number
