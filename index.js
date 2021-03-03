6cbaab84f05e653e8f7d47b6438edb20cdd2d729
Matthew Freeman <matthew.freeman821@gmail.com>
2021-03-03 08:58:41 -0500

    Adds highscore variable

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
