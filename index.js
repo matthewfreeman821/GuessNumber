6e0db8d16bfa4c6cf3ccf2e59c32e6c27c0fa061
Matthew Freeman <matthew.freeman821@gmail.com>
2021-03-07 11:59:39 -0500

    Replaces message where needed

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
   // No input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number';
+    message.textContent = '⛔️ No number';
   }
   // Guess is correct
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    message.textContent = '🎉 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
@@ -31,11 +31,11 @@ document.querySelector('.check').addEventListener('click', function () {
   // Guess is wrong
   else if (guess != secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      message.textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!'
+      message.textContent = '💥 You lost the game!'
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -45,7 +45,7 @@ document.querySelector('.again').addEventListener('click', function () {
   console.log('Clicked again');
   score = 20;
   document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  message.textContent = 'Start guessing...';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guess').value = '';
   document.querySelector('.number').style.width = '15rem';
